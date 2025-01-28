6e2335e1ec483b5c19b7c0b4d1c436290aef1dd3
Rohit Singh <rohit@upstreamgs.com>
2025-01-28 13:27:59 +0530

    basic-structure-added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'main';
+
+  elementControl:string = '';
+  selectGrid:string = '';
+  elementList:Array<string> = [
+    'Select', 'Input', 'Dropdown', 'MultiSelect', 'Radio Button', 'Checkboxes', "Upload"
+  ];
+  gridLineList:Array<string> = [
+   'Select', '3', '4', '6',
+  ];
+
+  contentGeneratedList:Array<object> = [];
+
+
+
+  handleGrid(event){
+    this.contentGeneratedList.push(
+      {name:this.elementControl, size:this.selectGrid, value:'', label:'', array:[]}
+    );
+
+    this.selectGrid = 'Select';
+
+  }
+
+
 }
