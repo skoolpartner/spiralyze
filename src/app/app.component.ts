7d9bc3524c1d98ce69f2e751241727b21c5ef646
Rohit Singh <rohit@upstreamgs.com>
2025-01-28 18:19:13 +0530

    added_input, multi_choice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,28 +7,82 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'main';
-
+  displayStyle:string = "none"; 
   elementControl:string = '';
   selectGrid:string = '';
   elementList:Array<string> = [
-    'Select', 'Input', 'Dropdown', 'MultiSelect', 'Radio Button', 'Checkboxes', "Upload"
+    'Select', 'Input',  'MultiChoice', 'Checkboxes'
   ];
   gridLineList:Array<string> = [
-   'Select', '3', '4', '6',
+   'Select', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'
   ];
 
-  contentGeneratedList:Array<object> = [];
+  contentGeneratedList:Array<any> = [
+    {
+      row:1,
+      elementRecord:[]
+    }
+  ];
 
 
+  // Assiging new element under individual row after 12Grid
 
-  handleGrid(event){
-    this.contentGeneratedList.push(
-      {name:this.elementControl, size:this.selectGrid, value:'', label:'', array:[]}
-    );
+  handleGrid(event){   
+   
+    for(var i=0; i<this.contentGeneratedList.length; i++){
 
-    this.selectGrid = 'Select';
+      var count:any = 0;
+      if(this.contentGeneratedList[i].elementRecord.length == 0){
+        this.contentGeneratedList[i].elementRecord.push({name:this.elementControl, size:this.selectGrid, value:'', label:'', array:[]})
+      }
+
+      else{
+        for(var x = 0; x<this.contentGeneratedList[i].elementRecord.length; x++){
+          count = count + Number(this.contentGeneratedList[i].elementRecord[x].size);
+        }
+        if(count < 12){
+          this.contentGeneratedList[i].elementRecord.push({name:this.elementControl, size:this.selectGrid, value:'', label:'', array:[]})
+        }
+        else{
+          debugger
+          if(count >= 12 && i == this.contentGeneratedList.length-1){
+            
+            this.contentGeneratedList.push(
+              {
+                row:i+2,
+                elementRecord:[{name:this.elementControl, size:this.selectGrid, value:'', label:'', array:[]}]
+              }
+            )
+            break
+          }
+
+          if(count + Number(this.selectGrid) > 12 && i == this.contentGeneratedList.length-1){
+            
+            this.contentGeneratedList.push(
+              {
+                row:i+2,
+                elementRecord:[{name:this.elementControl, size:this.selectGrid, value:'', label:'', array:[]}]
+              }
+            )
+            break
+          }
+         
+        }
+        
+      }
+      
+    }
+
 
   }
 
+
+  removeElement(index:number, childIndex:number){
+    this.contentGeneratedList[index].elementRecord.splice(childIndex, 1);
+  }
+
+  openElement(index:number, childIndex:number){
+    this.displayStyle = 'block';
+  }
 
 }
